aba4ae63374a0399bafd5a59344aa0e13d555aa5
Jarek Jakubowski <jarjak@tsh.io>
2019-10-08 17:28:44 +0200

    Fix Bar

--- src/Foo/Bar.php
+++ src/Foo/Bar.php
@@ -1,34 +1,31 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Foo;
 
 class Bar
 {
-    /**
-     * @param array $input
-     * @return array
-     */
-    function increment_all($input) {
-        $output = array();
+    public function incrementAll(array $input): array
+    {
+        $output = [];
         foreach ($input as $i => $value) {
-            if(is_numeric($value) == true) {
+            if (true === is_numeric($value)) {
                 $output[$i] = $value + 1;
             } else {
                 $output[$i] = $value;
             }
         }
+
         return $output;
     }
 
-    /**
-     * @param string $input
-     * @param string $separator
-     * @param array|null $allowedChars
-     * @return array
-     */
-    public function slugify($input, $separator = '-') {
-        $input = strtolower($input);
-        $input = preg_replace("/[^a-z0-9]/", $separator, $input);
+    public function slugify(string $input, string $separator = '-'): string
+    {
+        $input = mb_strtolower($input);
+        $input = preg_replace('/[^a-z0-9]/', $separator, $input);
         $input = preg_replace('/[\\'.$separator.']+/', $separator, $input);
+
         return trim($input, $separator);
     }
 }
